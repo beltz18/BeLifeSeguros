c1f85bf8f4c5498e566f62915bb7f30d61db3915
beltz18 <beltz.18kyodai@gmail.com>
2022-05-05 06:40:54 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+//dependencies
+const express = require("express")
+const app     = express()
+const ejs     = require("ejs")
+const parser  = require("body-parser")
+const path    = require("path")
+//declarations
+const port    = 4000
+
+//middlewares
+app.use(parser.urlencoded({extended:true}))
+app.set("views", path.join(__dirname, "/views"))
+app.engine("ejs", ejs.__express)
+app.set("view engine", "ejs")
+
+app.listen(port, () =>  {
+  console.log(`Server running on http://localhost:${port}`)
+})
+
+app.get('/', (req,res) => {
+  res.render("index")
+})
